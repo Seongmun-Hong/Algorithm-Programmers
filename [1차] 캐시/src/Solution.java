3c31073180693b79ad06f784dcf3fdc3a1a63131
Seongmun-Hong <hsm63746244@gmail.com>
2019-04-02 17:43:40 +0900

    add :  - 1차 캐시 문제 생성

---
+++ [1차] 캐시/src/Solution.java
@@ -0,0 +1,12 @@
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int solution(int cacheSize, String[] cities) {
+        int answer = 0;
+        return answer;
+    }
+
+}
