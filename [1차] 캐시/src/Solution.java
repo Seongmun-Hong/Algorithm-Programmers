0cfba6e9cc960995056b8a0c7b5a583cd15b035d
Seongmun-Hong <hsm63746244@gmail.com>
2019-04-03 16:20:44 +0900

    add :  - 1차 캐시 문제 풀이 추가

--- [1차] 캐시/src/Solution.java
+++ [1차] 캐시/src/Solution.java
@@ -1,11 +1,45 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Solution {
 
     public static void main(String[] args) {
 
+        String [] cities = {"Jeju", "Pangyo", "Seoul", "NewYork", "LA", "Jeju", "Pangyo", "Seoul", "NewYork", "LA"};
+        int cacheSize = 3;
+
+        int time = solution(cacheSize, cities);
+        System.out.println("" + time);
     }
 
-    public int solution(int cacheSize, String[] cities) {
+    public static int solution(int cacheSize, String[] cities) {
+
         int answer = 0;
+
+        Queue<String> citiesQueue = new LinkedList<>();
+
+        if(cacheSize == 0) {
+            return cities.length * 5;
+        }
+
+        for (int i = 0; i < cities.length; i++) {
+            cities[i] = cities[i].toUpperCase();
+//            System.out.println("word : " + cities[i]);
+            boolean contains = citiesQueue.contains(cities[i]);
+            if (contains) {
+                citiesQueue.remove(cities[i]);
+                citiesQueue.add(cities[i]);
+                answer += 1;
+            } else {
+                if(citiesQueue.size() >= cacheSize) {
+                    citiesQueue.poll();
+                }
+                citiesQueue.add(cities[i]);
+                answer += 5;
+            }
+//            System.out.println(citiesQueue.toString());
+        }
+
         return answer;
     }
 
