08f2d2c0adb500bbca5d0e405ebe9760594b35cd
Seongmun-Hong <hsm63746244@gmail.com>
2019-04-18 14:15:42 +0900

    add :  - 1차 프렌즈4블록 추가

---
+++ [1차] 프렌즈4블록/src/Solution.java
@@ -0,0 +1,24 @@
+public class Solution {
+
+    public static void main(String[] args) {
+
+        int m = 4;
+        int n = 5;
+        String [] board = {"CCBDE", "AAADE", "AAABF", "CCBBF"};
+        System.out.println(solution(m, n, board));
+    }
+
+    public static int solution(int m, int n, String[] board) {
+
+        int answer = 0;
+
+        for (int i = 0; i < m-1; i++) {
+
+            
+
+        }
+
+        return answer;
+    }
+
+}
